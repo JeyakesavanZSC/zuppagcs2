4a1e94fb3df73d561821d69f076786ec1e64613e
arul <mailtoarull@gmail.com>
2023-06-16 15:21:35 +0530

    append previous commit

--- firebase.js
+++ firebase.js
@@ -1,22 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getReactNativePersistence } from "firebase/auth/react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-// Initialize Firebase
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCb_gaT2wxEFNQSviATM3mmg9gkNF85qWU",
-//   authDomain: "zuppagcs.firebaseapp.com",
-//   projectId: "zuppagcs",
-//   storageBucket: "zuppagcs.appspot.com",
-//   messagingSenderId: "18369303417",
-//   appId: "1:18369303417:web:2fcce62e53b8aced4315a8",
-// };
+Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDW7v6Yd98INBab1iionCfSgVgyTeoQ_20",
-  authDomain: "meta-glazing-388812.firebaseapp.com",
-  projectId: "meta-glazing-388812",
-  storageBucket: "meta-glazing-388812.appspot.com",
-  messagingSenderId: "796688891913",
-  appId: "1:796688891913:web:fcaad55a98e2fb2cd278d8",
+  apiKey: "AIzaSyCb_gaT2wxEFNQSviATM3mmg9gkNF85qWU",
+  authDomain: "zuppagcs.firebaseapp.com",
+  projectId: "zuppagcs",
+  storageBucket: "zuppagcs.appspot.com",
+  messagingSenderId: "18369303417",
+  appId: "1:18369303417:web:2fcce62e53b8aced4315a8",
 };
 
 const app = initializeApp(firebaseConfig, {
