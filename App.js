309eb8dbf93124c4c2f44ff86b03b611ed46e655
Jeyakesavan <jeyakesavanzuppa@gmail.com>
2023-06-21 13:12:00 +0530

    21/06 main5

--- App.js
+++ App.js
@@ -2,8 +2,8 @@ import "react-native-gesture-handler";
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { AppRegistry } from "react-native";
+import {} from "./firebase";
 
-//import { firebase } from './config';
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -14,33 +14,10 @@ import Map from "./Screens/Map";
 const Stack = createStackNavigator();
 
 import { Text, View, NativeBaseProvider } from "native-base";
-
+import { useAuthentication } from "./utils/hooks/useAuthentication";
 
 export default function App() {
-  /*
-
-  const [initialization,setInitialization] =useState(true);
-
-  function onAuthStateChanged(user){
-  setUserName(user);
-  if(initializing)
-  setInitialization(false);
-  }
-
-  useEffect(()=>{
-    const subscriber=firebase.auth().onAuthStateChanged(onAuthStateChanged);
-    return subscriber;
-  },[])
-
-  if(initializing)
-  setInitialization(false);
-
-  if(!user)
-  {
-
-  }
-
-*/
+ 
   const { user } = useAuthentication();
   useEffect(() => {
     console.log("auth_user", user);
@@ -64,21 +41,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-// Color Switch Component
-// function ToggleDarkMode() {
-//   const { colorMode, toggleColorMode } = useColorMode();
-//   return (
-//     <HStack space={2} alignItems="center">
-//       <Text>Dark</Text>
-//       <Switch
-//         isChecked={colorMode === "light"}
-//         onToggle={toggleColorMode}
-//         aria-label={
-//           colorMode === "light" ? "switch to dark mode" : "switch to light mode"
-//         }
-//       />
-//       <Text>Light</Text>
-//     </HStack>
-//   );
-// }
