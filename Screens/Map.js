fe829bd0e7fc05648dd9082b283ced66ba39ef59
Jeyakesavan <jeyakesavanzuppa@gmail.com>
2023-06-24 17:29:32 +0530

    24/06 code

--- Screens/Map.js
+++ Screens/Map.js
@@ -1,32 +1,60 @@
-import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import {  StyleSheet } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 
-const Map = () => {
+export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
- 
+  useEffect(() => {
+    (async () => {
+      // Request permission to access the device's location
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+      // Get the device's current location
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let lati = 'Waiting..';
+  let long='';
+  if (errorMsg) {
+    lati = errorMsg;
+  } else if (location) {
+    lati = location.coords.latitude;
+    long= location.coords.longitude;
+  }
+  
   const [mapRegion, setmapRegion] = useState({
-    latitude: 12.96244,
-    longitude: 80.26270,
+    latitude: 12.9630,
+    longitude: 80.2646,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   });
-  
-  
+
   return (
-    <View style={styles.container}>
-      <MapView
+  
+      <View style={styles.container}>
+     <MapView
         style={{ alignSelf: 'stretch', height: '100%' }}
-
-
-        region={mapRegion}
-      >
+        region={mapRegion}>
+      
         <Marker coordinate={mapRegion} title='Marker' pincolor="gold"/>
+        <Marker coordinate={{latitude:lati,longitude:long}}
+        image={require('../images/uav.png')}
+        title='ZUPPA DRONE'/>  
       </MapView>
-    </View>
+      </View>
   );
-};
-export default Map;
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
